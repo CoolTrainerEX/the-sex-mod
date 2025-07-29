236eaa19a11d9ab643f051e90f349a63e5ad9159
CoolTrainerEX <92789937+CoolTrainerEX@users.noreply.github.com>
2025-07-29 19:52:32 +0800

    need gui

--- src/main/java/com/we1rdoyt/item/ConsentItem.java
+++ src/main/java/com/we1rdoyt/item/ConsentItem.java
@@ -3,14 +3,19 @@ package com.we1rdoyt.item;
 import java.util.UUID;
 
 import com.we1rdoyt.component.ModDataComponentTypes;
+import com.we1rdoyt.entity.effect.ModStatusEffects;
+import com.we1rdoyt.entity.effect.STDStatusEffect;
 
 import net.minecraft.component.ComponentChanges;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.Registries;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -26,12 +31,12 @@ public class ConsentItem extends Item {
     public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
         World world = user.getWorld();
 
-        if (world.isClient() || !entity.isLiving() || !stack.isOf(this))
+        if (world.isClient() || !entity.isLiving() || entity.getCommandTags().contains("sex"))
             return ActionResult.PASS;
 
         if (entity.isPlayer()) {
             stack.decrementUnlessCreative(1, user);
-            playerToPlayer(user, (PlayerEntity) entity);
+            playerToPlayer(user, (PlayerEntity) entity, (ServerWorld) world);
         } else if (user.isSneaking()) {
             ItemStack newStack = ItemUsage.exchangeStack(stack, user, new ItemStack(Registries.ITEM.getEntry(this), 1,
                     ComponentChanges.builder().add(ModDataComponentTypes.TARGET_ENTITY, entity.getUuid()).build()));
@@ -49,24 +54,86 @@ public class ConsentItem extends Item {
             }
 
             stack.decrement(1);
-            entityToEntity((LivingEntity) world.getEntity(uuid), (LivingEntity) entity);
+            entityToEntity((LivingEntity) world.getEntity(uuid), (LivingEntity) entity, (ServerWorld) world);
         } else {
             stack.decrementUnlessCreative(1, user);
-            playerToEntity(user, (LivingEntity) entity);
+            playerToEntity(user, (LivingEntity) entity, (ServerWorld) world);
         }
 
         return ActionResult.SUCCESS.withNewHandStack(stack);
     }
 
-    private static void playerToPlayer(PlayerEntity player, PlayerEntity target) {
+    private static void playerToPlayer(PlayerEntity player, PlayerEntity target, ServerWorld world) {
+        if (!getPlayerConsent(target)) {
+            noConsentParticles(target, world);
+            return;
+        }
+
+        startSex(player, target);
+        endSex(player, target);
+    }
+
+    private static void playerToEntity(PlayerEntity player, LivingEntity entity, ServerWorld world) {
+        if (!getEntityConsent(entity)) {
+            noConsentParticles(entity, world);
+            return;
+        }
+
+        startSex(player, entity);
+        endSex(player, entity);
+    }
+
+    private static void entityToEntity(LivingEntity entity, LivingEntity target, ServerWorld world) {
+        if (!getEntityConsent(entity)) {
+            noConsentParticles(entity, world);
+            return;
+        }
+
+        if (!getEntityConsent(target)) {
+            noConsentParticles(target, world);
+            return;
+        }
+
+        startSex(entity, target);
+        endSex(entity, target);
+    }
+
+    private static boolean getPlayerConsent(PlayerEntity target) {
+        return true;
+    }
+
+    private static boolean getEntityConsent(LivingEntity entity) {
+        double chance = 0.5;
+
+        if (entity.hasStatusEffect(ModStatusEffects.LIBIDO))
+            chance += (entity.getStatusEffect(ModStatusEffects.LIBIDO).getAmplifier() + 1) * 0.25;
+
+        return entity.getRandom().nextDouble() < chance;
+    }
+
+    private static void noConsentParticles(LivingEntity entity, ServerWorld world) {
+        world.spawnParticles(ParticleTypes.ANGRY_VILLAGER, entity.getParticleX(0.5), entity.getBodyY(1),
+                entity.getParticleZ(0.5), 5, 0.25, 0.25, 0.25, 1);
 
     }
 
-    private static void playerToEntity(PlayerEntity player, LivingEntity entity) {
-
+    private static boolean startSex(LivingEntity entity, LivingEntity target) {
+        entity.addCommandTag("sex");
+        target.addCommandTag("sex");
+        return entity.startRiding(target, true);
     }
 
-    private static void entityToEntity(LivingEntity entity, LivingEntity target) {
+    private static void endSex(LivingEntity entity, LivingEntity target) {
+        entity.removeCommandTag("sex");
+        target.removeCommandTag("sex");
+        entity.stopRiding();
 
+        if (entity.hasStatusEffect(ModStatusEffects.STD)) {
+            StatusEffectInstance statusEffectInstance = entity.getStatusEffect(ModStatusEffects.STD);
+
+            if (target.getRandom().nextDouble() < ((STDStatusEffect) statusEffectInstance.getEffectType())
+                    .getInfectChance() + (statusEffectInstance.getAmplifier() + 1) * 0.25)
+                target.addStatusEffect(statusEffectInstance);
+        }
     }
 }
